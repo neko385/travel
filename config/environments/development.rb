6e2278c8455fc0ea4f8aaa265eca63446b923cd6
neko385 <102905708+neko385@users.noreply.github.com>
2022-07-02 08:52:38 +0000

    devise導入,Admin・Customerモデル作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "295f7fb9088a4ac3a39659a42d709aac.vfs.cloud9.us-east-1.amazonaws.com"
 end
