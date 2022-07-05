d19d54851ce312b0e3cf812bad874d094d1beb54
neko385 <102905708+neko385@users.noreply.github.com>
2022-07-05 08:43:34 +0000

    [Add]public/customer_controller編集,viewファイル編集

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,27 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = Customer.find(params[:id])
+    @travel_memories = @customer.travel_memories
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to public_customer_path
+    else
+      render :edit
+    end
   end
 
   def confirm
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:screen_name, :last_name, :first_name, :last_kana, :first_kana, :email)
   end
 end
