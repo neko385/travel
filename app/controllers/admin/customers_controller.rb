79a05ccafe2cbedbd38b936b9e16280f7ff821ff
neko385 <102905708+neko385@users.noreply.github.com>
2022-07-07 06:10:45 +0000

    [Add]管理者側customer編集

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,27 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:screen_name, :last_name, :first_name, :last_kana, :first_kana, :email, :is_deleted)
   end
 end
