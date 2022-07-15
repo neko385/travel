5dac0d10ea69c7108c4630d835bf61a75d22d2af
neko385 <102905708+neko385@users.noreply.github.com>
2022-07-15 15:07:51 +0000

    [Update]コメント機能非同期通信完了

--- app/controllers/public/travel_memory_comments_controller.rb
+++ app/controllers/public/travel_memory_comments_controller.rb
@@ -1,15 +1,13 @@
 class Public::TravelMemoryCommentsController < ApplicationController
   def create
     @travel_memory = TravelMemory.find(params[:travel_memory_id])
-    @comment = current_customer.travel_memory_comments.new(travel_memory_comment_params)
-    @comment.travel_memory_id = @travel_memory.id
-    @comment.save
-    flash.now[:notice] = 'コメントを投稿しました'
+    comment = current_customer.travel_memory_comments.new(travel_memory_comment_params)
+    comment.travel_memory_id = @travel_memory.id
+    comment.save
   end
 
   def destroy
     TravelMemoryComment.find(params[:id]).destroy
-    flash.now[:alert] = 'コメントを削除しました'
     @travel_memory = TravelMemory.find(params[:travel_memory_id])
   end
 
