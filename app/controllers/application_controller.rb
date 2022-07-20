327d6dba43adb7483f01febe2f14fada5a8b26c1
neko385 <102905708+neko385@users.noreply.github.com>
2022-07-20 16:57:10 +0000

    [Fix]マップ機能,レイアウト編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_customer!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  # 管理者側ログイン後
+  def after_admin_sign_in_path_for(resource)
+    admin_customers_path
+  end
+
+  # 顧客側ログアウト後
+  def after_admin_sign_out_path_for(resource)
+    new_admin_session_path
+  end
+
   # 顧客側ログイン後
   def after_public_sign_in_path_for(resource)
     public_travel_memories_path
