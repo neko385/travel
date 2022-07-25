cffc3709221021dec89afd46350762844d380f64
neko385 <102905708+neko385@users.noreply.github.com>
2022-07-25 11:45:55 +0000

    [Fix]コメント機能

--- app/models/travel_memory_comment.rb
+++ app/models/travel_memory_comment.rb
@@ -3,4 +3,6 @@ class TravelMemoryComment < ApplicationRecord
   belongs_to :customer
 
   validates :comment, presence: true
+  
+  scope :active, -> { joins(:customer).where(customer: {is_deleted: false}) }
 end
